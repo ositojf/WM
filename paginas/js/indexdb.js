cc55f01d746273f6b281b8f66ff92f9a298ba683
ositojf <ositojf00@gmail.com>
2022-10-31 20:25:31 +0100

    wm

---
+++ paginas/js/indexdb.js
@@ -0,0 +1,26 @@
+
+//CONEXION 
+const dbConnection = window.indexedDB.open('registro', 2);                                   // version 1
+let db;
+//on success = El successevent se dispara cuando un IDBRequesttiene éxito.
+dbConnection.onsuccess = () => {
+    db = dbConnection.result;
+    console.log("Base de datos abierta", db);
+}
+// El upgradeneededevento se activa cuando se intentó abrir una base de datos 
+// con un número de versión superior a su versión actual.
+dbConnection.onupgradeneeded = (e) => {
+    db = e.target.result;//elemento que nos devuelve
+    console.log("Crear objetos de DB", db);
+    const coleccionPersona = db.createObjectStore('persona', {
+        keyPath: 'idPersona'
+    });
+    const coleccionCompras = db.createObjectStore('compras', {
+        keyPath: 'idCompras'
+    });
+}
+// El errorevento se activa IDBTransactioncuando una solicitud devuelve un error 
+// y el evento aparece en el objeto de la transacción.
+dbConnection.onerror = (error) =>{
+    console.log(error);
+}
